c4a094eadebcf04b50c36c6e17a018cefc88b239
alieAlaeva <alien.ablaeva.03@gmail.com>
2024-06-25 02:06:08 +0300

    Add python script and config for sampling data

---
+++ env/lib/python3.10/site-packages/voluptuous/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+# fmt: off
+from voluptuous.schema_builder import *
+from voluptuous.util import *
+from voluptuous.validators import *
+
+from voluptuous.error import *  # isort: skip
+
+# fmt: on
+
+__version__ = '0.15.0'
+__author__ = 'alecthomas'
